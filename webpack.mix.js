022a1cba29e383cb7a391ebca45cad60c17c956a
Swithbasement <thestarswith@gmail.com>
2023-04-07 22:48:12 +0700

    add firebase config by npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const mix = require('laravel-mix');
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDDE2n8PjJTZB1ASSVFbPSmdjyjZybe1K8",
+    authDomain: "taxiapp-21306.firebaseapp.com",
+    databaseURL: "https://taxiapp-21306-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "taxiapp-21306",
+    storageBucket: "taxiapp-21306.appspot.com",
+    messagingSenderId: "840248590536",
+    appId: "1:840248590536:web:be841f1a7a5f1c4fede5fd",
+    measurementId: "G-LWZD594C6Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 /*
  |--------------------------------------------------------------------------
